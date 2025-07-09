679bd901c5fd30b7fce726ff05bd2dfaf9d71c01
Juanes <hpchris20@gmail.com>
2025-07-08 23:44:21 -0500

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import HomePage from './features/paginas/HomePage';
 import LandingPage from './features/paginas/LandingPage';
 import CartPage from './features/paginas/CartPage';
 import Navbar from './shared/components/Navbar';
-import { CartProvider } from './features/context/CartContext';
 import Footer from './shared/components/Footer';
 import Login from './features/auth/components/Login';
 import Registro from './features/auth/components/Registro';
@@ -14,22 +13,20 @@ import './shared/styles/styles.css';
 function App() {
   return (
     <BrowserRouter>
-      <CartProvider>
-        <Navbar />
-        <main style={{ paddingTop: '80px', minHeight: 'calc(100vh - 160px)', display: 'flex', flexDirection: 'column' }}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/productos" element={<LandingPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/registro" element={<Registro />} />
-            <Route element={<ProtectedRoute />}>
-              <Route path="/dashboard" element={<DashboardPage />} />
-            </Route>
-          </Routes>
-        </main>
-        <Footer/>
-      </CartProvider>
+      <Navbar />
+      <main style={{ paddingTop: '80px', minHeight: 'calc(100vh - 160px)', display: 'flex', flexDirection: 'column' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/productos" element={<LandingPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Route>
+        </Routes>
+      </main>
+      <Footer/>
     </BrowserRouter>
   );
 }
